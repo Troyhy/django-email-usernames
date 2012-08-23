6332b6239e2cbe741ed04675517e2983a3f4b038
troyhy <toni.royhy@designhouseilo.fi>
2012-08-23 14:52:54 +0300

    Added setup.py and README.rst

---
+++ README.rst
@@ -0,0 +1,2 @@
+Django-email-username
+=====================
---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='django-email-usernames',
+    version='0.2',
+    description="User's email address for login and authentication in Django.",
+    long_description=open('README.rst').read(),
+    author='Håkan Waara',
+    author_email='hwaara@gmail.com',
+    url='https://github.com/Troyhy/django-email-usernames',
+    packages=['email_usernames'],
+    package_dir={'email_usernames': 'email_usernames'},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
